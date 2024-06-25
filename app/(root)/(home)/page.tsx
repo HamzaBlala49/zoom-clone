d0d840f61460cd833c22fe7d1048cde4072178d3
Hamza Omar Balala <103255525+HamzaBlala49@users.noreply.github.com>
2024-06-25 18:23:47 +0300

    fix state undefined in home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,10 @@
-"use client"
 import MeetingTypeList from '@/components/MeetingTypeList';
-import { useGetCalls } from '@/hooks/useGetCalls';
-import { Call } from '@stream-io/video-react-sdk';
+// import { useGetCalls } from '@/hooks/useGetCalls';
+// import { Call } from '@stream-io/video-react-sdk';
 
 
 const Home = () => {
-  const { upcomingCalls } = useGetCalls()
+  // const { upcomingCalls } = useGetCalls()
   const now = new Date();
   const time = now.toLocaleTimeString("en-US", {
     hour:"2-digit",
@@ -20,10 +19,10 @@ const Home = () => {
     <section className='flex size-full flex-col gap-10 text-white'>
       <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
         <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-          <h2 className='glassmorphism max-w-[370px] rounded py-2 text-center text-base font-normal'>
+          {/* <h2 className='glassmorphism max-w-[370px] rounded py-2 text-center text-base font-normal'>
             Upcoming Meeting at {" "}
             {(upcomingCalls.reverse()[0] as Call)?.state?.startsAt?.toLocaleString()}
-          </h2>
+          </h2> */}
           <div className='flex flex-col gap-2'>
             <h1 className='text-4xl font-extrabold lg:txt-7xl'>
                 {time}
